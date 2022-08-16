70f8d32e6faafbce5450762c823cc07bc9818f90
Viktoriya Danilova <v_kozulya@mail.ru>
2022-08-16 21:03:17 +0500

    finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minimum = 0;
+        this.maximum = 0;
+    }
 
-    setRange(min, max) {
-
+    setRange(minimum, maximum) {
+        this.minimum = minimum;
+        this.maximum = maximum;
     }
 
     guess() {
-
+        this.value = Math.ceil(this.minimum + (this.maximum - this.minimum)/2);
+        return this.value;
     }
 
     lower() {
-
+        this.maximum = this.guess();
     }
 
     greater() {
-
+        this.minimum = this.guess();
     }
 }
 
